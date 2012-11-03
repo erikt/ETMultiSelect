18aa590e40b976bb7f0ae3c6915c4d80ba993abb
Erik Tjernlund <erik@tjernlund.net>
2012-11-03 23:57:38 +0100

    Initial Commit

---
+++ ETMultiSelect/ETAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ETAppDelegate.h
+//  ETMultiSelect
+//
+//  Created by Erik Tjernlund on 2012-11-03.
+//  Copyright (c) 2012 Erik Tjernlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ETViewController;
+
+@interface ETAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ETViewController *viewController;
+
+@end
---
+++ ETMultiSelect/ETViewController.h
@@ -0,0 +1,13 @@
+//
+//  ETViewController.h
+//  ETMultiSelect
+//
+//  Created by Erik Tjernlund on 2012-11-03.
+//  Copyright (c) 2012 Erik Tjernlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ETViewController : UIViewController
+
+@end
